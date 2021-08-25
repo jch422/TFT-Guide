1a9a12ab1d41ff942cb76310b07cb3efd665fe0f
jc <jotace422@gmail.com>
2021-08-25 14:30:16 +0900

    Style: Prettier, Eslint를 적용하여 index.js 코드 포맷 수정

--- server/index.js
+++ server/index.js
@@ -7,13 +7,12 @@ const DEV_CLIENT = process.env.DEV_CLIENT;
 app.use(express.json());
 
 app.use(
-    cors({
-      origin: [DEV_CLIENT],
-      credentials: true,
-      methods: ["GET", "POST","PUT","DELETE","OPTIONS"],
-    })
-  );
-
+  cors({
+    origin: [DEV_CLIENT],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }),
+);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
