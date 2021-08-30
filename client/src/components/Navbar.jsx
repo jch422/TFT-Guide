bd333803e3bba470b16e02b09faf27f0069248e8
jc <jotace422@gmail.com>
2021-08-31 08:46:48 +0900

    Feat: react-router를 사용하여 라우팅 기능 구현

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,46 +1,61 @@
-import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-function Navbar(props) {
-  const Header = styled.header`
-    // styled 어떻게 써야되지???
-    height: 5vh;
-    font-size: 5vh;
-    align-items: center;
-    background-color: #0d2e41;
-    display: flex;
-    justify-content: center;
-  `;
-  const Span = styled.span`
-    font-size: 4vh;
-    font-family: Impact;
-    color: #fbed0b;
-    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000,
-      4px 2px 0px #00f0ff, 8px 4px 0px #80ff02;
-    padding: 1vh;
-    align-content: center;
-    &:hover {
-      color: #dc44c3;
-      cursor: pointer;
-    }
-  `;
-  const goToPage = pageName => {
-    props.setCurrent(pageName);
-    console.log(props.current);
-  };
+import MainPage from '../pages/MainPage';
+import MyPage from '../pages/MyPage';
+import HistorySearch from '../pages/HistorySearch';
+import Login from '../pages/Login';
+import UserInfo from '../components/UserInfo';
+
+const Navbar = () => {
   return (
-    <Header>
-      <Span className="toMain" onClick={() => goToPage('main')}>
-        Main
-      </Span>
-      <Span className="toMypage" onClick={() => goToPage('mypage')}>
-        Mypage
-      </Span>
-      <Span className="toSearch" onClick={() => goToPage('search')}>
-        Search
-      </Span>
-    </Header>
+    <Router>
+      <Header>
+        <StyledLink to="/">Main</StyledLink>
+        <StyledLink to="/mypage">MyPage</StyledLink>
+        <StyledLink to="/profile">Search</StyledLink>
+        <UserInfo />
+      </Header>
+
+      <Switch>
+        <Route path="/mypage">
+          <MyPage />
+        </Route>
+        <Route path="/profile">
+          <HistorySearch />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <MainPage />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
+
+const Header = styled.header`
+  height: 5vh;
+  font-size: 5vh;
+  align-items: center;
+  background-color: #0d2e41;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 4vh;
+  font-family: Impact;
+  color: #fbed0b;
+  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000,
+    4px 2px 0px #00f0ff, 8px 4px 0px #80ff02;
+  padding: 1vh;
+  align-content: center;
+  &:hover {
+    color: #dc44c3;
+    cursor: pointer;
+  }
+`;
 
 export default Navbar;
