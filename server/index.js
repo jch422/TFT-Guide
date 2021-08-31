1c73e85863f67a5fb739ef67c49f6a94f54c1661
hyeon3051 <hyeon3051@gmail.com>
2021-08-31 14:55:55 +0900

    Feat : 라우터 설정

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const app = express();
 const usersRouter = require('./routes/user');
 const decksRouter = require('./routes/deck');
 const matchesRouter = require('./routes/matches');
+const championRouter = require('./routes/champion');
 const { verifyAccessToken } = require('./middelware/access-token');
+const initialController = require('./routes/initial');
 
 app.use(express.json());
 app.use(
@@ -24,11 +26,13 @@ app.use(
   }),
 );
 app.use('/users', verifyAccessToken, usersRouter);
-app.use('/decks', decksRouter);
+app.use('/deck', decksRouter);
 app.use('/matches', matchesRouter);
+app.use('/champion', championRouter);
+app.use('/initial', initialController);
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
+app.listen(8080, () => {
+  console.log(`listening on port ${8080}`);
 });
 
 module.exports = app;
