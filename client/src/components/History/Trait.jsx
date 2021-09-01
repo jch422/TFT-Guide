6e59bd3f629dece675dd6aac1f9b7013d9982b51
Alamarama <pian14q@gmail.com>
2021-09-01 22:31:10 +0900

    Feat 마이페이지

---
+++ client/src/components/History/Trait.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import { SearchJSONData } from '../SearchJSONData';
+
+function Trait({ data }) {
+  let jsonData = SearchJSONData('traits', data.name);
+  let style = '';
+  for (let i of jsonData.sets) {
+    if (i.min <= data.num_units) {
+      style = i.style;
+    }
+  }
+  return (
+    <TraitsContainer>
+      <TraitContent style={{ backgroundImage: `url('../TFTData/traits/${style}.png')` }}>
+        <TraitImage src={`../TFTData/traits/${jsonData['name']}.svg`}></TraitImage>
+        <TooltipText>{data.num_units + jsonData['kr_name']}</TooltipText>
+      </TraitContent>
+    </TraitsContainer>
+  );
+}
+
+export default Trait;
+
+const TraitsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const TraitImage = styled.img`
+  display: block;
+  margin-top: 5px;
+  margin-right: 1px;
+  height: 20px;
+  width: 20px;
+  padding: 2px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const TooltipText = styled.span`
+  visibility: hidden;
+  width: auto;
+  height: 100%;
+  white-space: nowrap;
+  background-color: black;
+  color: white;
+  text-align: center;
+  border-radius: 5px;
+  padding: 10px 5px;
+  position: absolute;
+  z-index: 1;
+  top: 120%;
+  font-size: 13px;
+  :after {
+    content: '';
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    margin-left: -10px;
+    border-width: 10px;
+    border-style: solid;
+    border-color: transparent transparent black transparent;
+  }
+`;
+const TraitContent = styled.div`
+  box-sizing: border-box;
+  height: 30px;
+  width: 30px;
+  background-position: center;
+  background-size: 90%;
+  background-repeat: no-repeat;
+  position: relative;
+  display: inline-block;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  &:hover ${TooltipText} {
+    visibility: visible;
+  }
+`;
---
+++ client/src/components/Mypage/Deck.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import champions from '../../JSON/champions.json';
+import traits from '../../JSON/traits.json';
+import Trait from './MyTrait';
+import Unit from './Unit';
+
+const Deck = styled.div`
+  width: 80%;
+  margin: 10px;
+  list-style-type: none;
+  background: #17394f;
+  border: solid black 0.2rem;
+  display: flex;
+  flex-direction: row;
+  min-width: 500px;
+  padding: 8px;
+`;
+const Name = styled.span`
+  flex: 0 1 10%;
+  color: white;
+  display: flex;
+  align-self: center;
+`;
+const Synergy = styled.span`
+  flex: 2 1 30%;
+  display: flex;
+  align-self: center;
+`;
+const Champ = styled.span`
+  flex: 3 1 60%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+`;
+
+function DeckBox({ data, index }) {
+  const mydeck = data;
+  let set = new Set(mydeck);
+
+  let slimMydeck = [...set];
+
+  // 챔피온 배열에서 중복제거하기 추가
+
+  //  선택된 챔피언에 속한 트레잇을 객체 저가
+  //    해당 객체의 카운트를 보고 색깔 정해서 객체 저장
+  //    객체를 표시하는 코드에 맞게 변형해서 전달해주기
+  let traitsAll = [];
+  //트레잇을 모두 배열에 넣기
+  for (let i in slimMydeck) {
+    for (let j = 0; j < Object.keys(champions).length; j++) {
+      if (slimMydeck[i] === champions[j].championId) {
+        traitsAll.push(champions[j].traits);
+      }
+    }
+  }
+  console.log(traitsAll);
+  // 이차원배열 풀기
+  let traitsAllList = traitsAll.reduce(function (acc, cur) {
+    return acc.concat(cur);
+  });
+  console.log(traitsAllList);
+  let traitsObj = {};
+  // 중첩 카운트 객체로 만들기
+  for (let i = 0; i < traitsAllList.length; i++) {
+    if (!traitsObj[traitsAllList[i]]) {
+      traitsObj[traitsAllList[i]] = 1;
+    } else {
+      traitsObj[traitsAllList[i]]++;
+    }
+  }
+  console.log(traitsObj);
+  // 트레잇 색깔 찾기
+  let finalTraitsList = [];
+  for (let i in traitsObj) {
+    let color = '';
+    let traitKr = '';
+    let traitEn = '';
+    for (let j = 0; j < Object.keys(traits).length; j++) {
+      if (i === traits[j].key) {
+        for (let k = 0; k < traits[j].sets.length; k++) {
+          if (traitsObj[i] >= traits[j].sets[k].min) {
+            color = traits[j].sets[k].style;
+            traitKr = traits[j].kr_name;
+            traitEn = traits[j].key;
+          }
+        }
+      }
+    }
+    if (color) {
+      let str = traitEn + '+' + color + '+' + traitKr;
+      finalTraitsList.push(str);
+    }
+  }
+  console.log(finalTraitsList);
+
+  return (
+    <Deck>
+      <Name>{`${index + 1}번`}</Name>
+      <Synergy>
+        {finalTraitsList.map((data, index) => (
+          <Trait key={index} data={data} />
+        ))}
+      </Synergy>
+      <Champ>
+        {mydeck.map((el, index) => {
+          // const imgsrc = '../TFTData/champions/' + el + '.png';
+          // return <img alt="" src={imgsrc} width="50" height="50" />;
+          return <Unit key={index} el={el} />;
+        })}
+      </Champ>
+    </Deck>
+  );
+}
+
+export default DeckBox;
