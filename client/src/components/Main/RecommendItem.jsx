afeeb3386f1d1c1a9b448ba78fb2ab4cfa6d9471
jc <jotace422@gmail.com>
2021-09-02 18:51:54 +0900

    Feat: 추천 챔프 컴포넌트 구현

---
+++ client/src/components/Main/RecommendItem.jsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+const RecommendItem = ({ kr_name, championId, cost, championData, handleRecommendItemClick }) => {
+  const color = costColorMapper[cost];
+
+  return (
+    <Wrapper onClick={() => handleRecommendItemClick(championData)}>
+      <ChampionContainer borderColor={color}>
+        <ChampionImg src={`../TFTData/champions/${championId}.png`} />
+        <ChampionCost bgColor={color}>${cost}</ChampionCost>
+        <ChampionName>{kr_name}</ChampionName>
+      </ChampionContainer>
+    </Wrapper>
+  );
+};
+
+const costColorMapper = {
+  1: '#808080',
+  2: '#11b288',
+  3: '#207ac7',
+  4: '#c440da',
+  5: '#ffb93b',
+};
+
+const Wrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  margin-bottom: 1rem;
+  padding: 1px;
+  transition: transform 0.2s;
+  & {
+    cursor: pointer;
+  }
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const ChampionContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border: 2px solid ${({ borderColor }) => borderColor};
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ChampionImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+`;
+
+const ChampionCost = styled.div`
+  position: absolute;
+  top: 0;
+  right: -2px;
+  color: white;
+  width: 30%;
+  background-color: ${({ bgColor }) => bgColor};
+  border-top-right-radius: 10px;
+  text-align: center;
+  font-size: 0.8rem;
+`;
+
+const ChampionName = styled.div`
+  position: absolute;
+  color: white;
+  bottom: 0.3rem;
+  font-size: 0.8rem;
+  font-weight: bold;
+  text-shadow: 2px 2px 1px black;
+`;
+
+export default RecommendItem;
---
+++ client/src/components/Main/RecommendList.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+import RecommendItem from './RecommendItem';
+
+const RecommendList = ({ champions, handleRecommendItemClick }) => {
+  const recommendItems = champions.map((champion, idx) => (
+    <RecommendItem
+      key={idx}
+      {...champion}
+      championData={champion}
+      handleRecommendItemClick={handleRecommendItemClick}
+    />
+  ));
+
+  return <RecommentItemsContainer>{recommendItems}</RecommentItemsContainer>;
+};
+
+export default RecommendList;
+
+const RecommentItemsContainer = styled.div`
+  min-width: 12rem;
+`;
