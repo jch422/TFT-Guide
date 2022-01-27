038381ef692b53379a5ec3ebc4f8d8539ead888e
jc <jotace422@gmail.com>
2022-01-28 04:43:50 +0900

    [Client] design: 모바일 화면에서는 챔프 추천리스트를 가로로 볼 수 있도록 css 수정

--- client/src/components/Main/RecommendList.jsx
+++ client/src/components/Main/RecommendList.jsx
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 import RecommendItem from './RecommendItem.jsx';
 
-const RecommendList = ({ champions, handleRecommendItemClick }) => {
+const RecommendList = ({ champions, handleRecommendItemClick, isDark }) => {
   const recommendItems = champions.map((champion, idx) => (
     <RecommendItem
       key={idx}
       {...champion}
+      isDark={isDark}
       championData={champion}
       handleRecommendItemClick={handleRecommendItemClick}
     />
@@ -19,4 +20,14 @@ export default RecommendList;
 
 const RecommentItemsContainer = styled.div`
   min-width: 12rem;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 1200px) {
+    justify-content: space-between;
+    flex-direction: row;
+    min-width: 500px;
+  }
+  @media (max-width: 550px) {
+    min-width: 460px;
+  }
 `;
