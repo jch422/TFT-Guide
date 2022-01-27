4ca2e46a313688753c7c4530a4a42d637deb14cc
jc <jotace422@gmail.com>
2022-01-28 04:45:02 +0900

    [Client] design: 모바일 화면에서 비율에 맞게 잘 보이도록 css 수정

--- client/src/components/Main/Slot.jsx
+++ client/src/components/Main/Slot.jsx
@@ -50,6 +50,22 @@ const Hexagon = styled.div`
   & {
     cursor: pointer;
   }
+  @media (max-width: 1200px) {
+    width: 50px;
+    height: 28.87px;
+    margin: 14.43px 0;
+    background-size: auto 57.735px;
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0px;
+      left: 0;
+      width: 50px;
+      height: 28.8675px;
+      z-index: 2;
+      background: inherit;
+    }
+  }
 `;
 
 const Shared = styled.div`
@@ -63,6 +79,18 @@ const Shared = styled.div`
   transform: scaleY(0.5774) rotate(-45deg);
   background: inherit;
   left: 14.64px;
+  @media (max-width: 1200px) {
+    position: absolute;
+    z-index: 1;
+    width: 35.36px;
+    height: 35.36px;
+    overflow: hidden;
+    -webkit-transform: scaleY(0.5774) rotate(-45deg);
+    -ms-transform: scaleY(0.5774) rotate(-45deg);
+    transform: scaleY(0.5774) rotate(-45deg);
+    background: inherit;
+    left: 7.32px;
+  }
 `;
 
 const HexTop = styled(Shared)`
@@ -80,6 +108,23 @@ const HexTop = styled(Shared)`
     transform-origin: 0 0;
     background: inherit;
     background-position: center top;
+  }
+  @media (max-width: 1200px) {
+    top: -17.6777px;
+    &:after {
+      content: '';
+      position: absolute;
+      width: 50px;
+      height: 28.86751345948129px;
+      -webkit-transform: rotate(45deg) scaleY(1.7321) translateY(-14.4338px);
+      -ms-transform: rotate(45deg) scaleY(1.7321) translateY(-14.4338px);
+      transform: rotate(45deg) scaleY(1.7321) translateY(-14.4338px);
+      -webkit-transform-origin: 0 0;
+      -ms-transform-origin: 0 0;
+      transform-origin: 0 0;
+      background: inherit;
+      background-position: center top;
+    }
   }
 `;
 
@@ -99,6 +144,23 @@ const HexBottom = styled(Shared)`
     background: inherit;
     background-position: center bottom;
   }
+  @media (max-width: 1200px) {
+    bottom: -17.6777px;
+    &:after {
+      content: '';
+      position: absolute;
+      width: 50px;
+      height: 28.86751345948129px;
+      -webkit-transform: rotate(45deg) scaleY(1.7321) translateY(-14.4338px);
+      -ms-transform: rotate(45deg) scaleY(1.7321) translateY(-14.4338px);
+      transform: rotate(45deg) scaleY(1.7321) translateY(-14.4338px);
+      -webkit-transform-origin: 0 0;
+      -ms-transform-origin: 0 0;
+      transform-origin: 0 0;
+      background: inherit;
+      background-position: center bottom;
+    }
+  }
 `;
 
 const ChampionName = styled.div`
@@ -114,13 +176,17 @@ const ChampionName = styled.div`
   color: white;
   font-weight: bold;
   text-shadow: 2px 2px 1px black;
+  @media (max-width: 1200px) {
+    top: 1.2rem;
+    font-size: 0.6rem;
+  }
 `;
 
 const HexagonEmpty = styled.div`
   position: relative;
   width: 100px;
   height: 57.74px;
-  background-color: ${({ isDark }) => (isDark ? '#cccccc' : '#1b374a')};
+  background-color: ${({ isDark }) => (isDark ? '#cccccc' : '#2C2C2C')};
   margin: 28.87px 0;
   &:before,
   &:after {
@@ -132,14 +198,36 @@ const HexagonEmpty = styled.div`
   }
   &:before {
     bottom: 100%;
-    border-bottom: 28.87px solid ${({ isDark }) => (isDark ? '#cccccc' : '#1b374a')};
+    border-bottom: 28.87px solid ${({ isDark }) => (isDark ? '#cccccc' : '#2C2C2C')};
   }
   &:after {
     top: 100%;
     width: 0;
-    border-top: 28.87px solid ${({ isDark }) => (isDark ? '#cccccc' : '#1b374a')};
+    border-top: 28.87px solid ${({ isDark }) => (isDark ? '#cccccc' : '#2C2C2C')};
   }
   & {
     cursor: pointer;
+  }
+  @media (max-width: 1200px) {
+    width: 50px;
+    height: 28.87px;
+    margin: 14.43px 0;
+    &:before,
+    &:after {
+      content: '';
+      position: absolute;
+      width: 0;
+      border-left: 25px solid transparent;
+      border-right: 25px solid transparent;
+    }
+    &:before {
+      bottom: 100%;
+      border-bottom: 14.43px solid ${({ isDark }) => (isDark ? '#cccccc' : '#2C2C2C')};
+    }
+    &:after {
+      top: 100%;
+      width: 0;
+      border-top: 14.43px solid ${({ isDark }) => (isDark ? '#cccccc' : '#2C2C2C')};
+    }
   }
 `;
