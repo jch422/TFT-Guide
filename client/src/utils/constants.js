2509b218b436f6c72b8a9fca1d32913f44ecfd62
jc <jotace422@gmail.com>
2022-02-01 08:53:00 +0900

    [Client] refactor: 재사용되는 함수 일부를 utils로 이동

---
+++ client/src/utils/constants.js
@@ -0,0 +1,15 @@
+export const REMOVE_ZONE = -1;
+export const EMPTY_SLOT = {
+  name: '',
+  kr_name: '',
+  championId: '',
+  cost: 0,
+  traits: [],
+};
+export const costColorMapper = {
+  1: '#808080',
+  2: '#11b288',
+  3: '#207ac7',
+  4: '#c440da',
+  5: '#ffb93b',
+};
