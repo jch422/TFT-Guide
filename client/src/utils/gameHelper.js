9e9a7165612b75f63d6474d4976066f2c8b72bee
jc <jotace422@gmail.com>
2022-02-08 06:43:31 +0900

    [Client] feat: 챔프 추천 기능을 server 대신 client 단에서 구현

---
+++ client/src/utils/gameHelper.js
@@ -0,0 +1,84 @@
+import traitsData from '../JSON/set6/traits.json';
+import championsData from '../JSON/set6/champions.json';
+
+export const getRecommendation = slots => {
+  const filteredSlots = slots.filter(removeEmpty);
+  if (!filteredSlots.length) return [];
+
+  const level = filteredSlots.length;
+  const uniqueSlots = filteredSlots.filter(removeRedundant);
+  const [min, max] = levelCostMapper[level];
+  let candidates = championsData
+    .filter(c => c.cost >= min && c.cost <= max)
+    .filter(c => !uniqueSlots.some(({ name }) => name === c.name))
+    .filter(c => c.name !== 'Veigar');
+  candidates = candidates
+    .map(candidate => {
+      const newSlots = uniqueSlots.concat(candidate);
+      const newTraits = extractTraits(newSlots);
+      candidate.score = calcTraitsScore(newTraits);
+      return candidate;
+    })
+    .sort((a, b) => b.score - a.score);
+
+  return candidates.slice(0, 5);
+};
+
+const removeEmpty = slot => slot.name;
+
+const removeRedundant = (slot, idx, arr) => arr.findIndex(s => s.name === slot.name) === idx;
+
+const extractTraits = slots => {
+  return slots.reduce((acc, { traits }) => {
+    traits.forEach(trait => {
+      if (acc[trait]) {
+        acc[trait]++;
+      } else {
+        acc[trait] = 1;
+      }
+    });
+    return acc;
+  }, {});
+};
+
+const calcTraitsScore = traits => {
+  const traitsArr = Object.entries(traits);
+  return traitsArr.reduce((acc, [trait, cnt]) => {
+    const { sets } = traitsData.filter(({ name }) => name === trait)[0];
+    for (const { style, min, max } of sets) {
+      if (max) {
+        if (cnt >= min && cnt <= max) {
+          return acc + scoreMapper[style];
+        }
+      } else {
+        // only min
+        if (cnt >= min) {
+          return acc + scoreMapper[style];
+        }
+      }
+    }
+    return acc;
+  }, 0);
+};
+
+const scoreMapper = {
+  bronze: 1,
+  silver: 3,
+  gold: 5,
+  chromatic: 7,
+};
+
+const levelCostMapper = {
+  1: [1, 1],
+  2: [1, 1],
+  3: [1, 2],
+  4: [2, 3],
+  5: [2, 3],
+  6: [3, 4],
+  7: [3, 4],
+  8: [3, 5],
+  9: [4, 5],
+  10: [4, 5],
+  11: [4, 5],
+  12: [4, 5],
+};
